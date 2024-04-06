52e2bcb9fa8fbb815a6bc859fb15cc0a45620ac3
suny-am <visualarea.1@gmail.com>
2024-04-06 15:10:21 +0200

    Add local development guard

--- TokenRequest.cs
+++ TokenRequest.cs
@@ -2,6 +2,7 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 
 namespace gh_api_token_provider
@@ -16,14 +17,40 @@ namespace gh_api_token_provider
         }
 
         [Function("TokenRequest")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
         {
+
+            HttpResponseData response;
+
+            bool isLocalDevelopment = Convert.ToBoolean(Environment.GetEnvironmentVariable("IsLocalDevelopment") ?? "false");
+
+            if (isLocalDevelopment)
+            {
+                response = req.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+                response.WriteString("Local development");
+
+                return response;
+            }
+
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
+            string keyVaultName = "gh-api-tokens";
+            string secretName = "GhApiToken";
+            string keyVaultUri = $"https://{keyVaultName}.vault.azure.net/";
+
+            SecretClient client = new(new Uri(keyVaultUri), new DefaultAzureCredential());
+
+            var secret = client.GetSecret(secretName);
+
+            byte[] decodedData = Convert.FromBase64String(secret.Value.Value);
+            string decodedString = System.Text.Encoding.UTF8.GetString(decodedData);
+
+            response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString(decodedString);
 
             return response;
         }
